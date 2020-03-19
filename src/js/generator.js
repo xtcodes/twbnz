412728ecf3fc2d6c971953080f46b17b514cd2c6
SaturnHafen <saturnhafen@gmail.com>
2020-03-19 20:29:08 +0100

    change from pixel-by-pixel copy to just overlaying the two images

--- src/js/generator.js
+++ src/js/generator.js
@@ -1,89 +1,85 @@
-var profilePicture 			= undefined;
-var imageData				= undefined;
-var max_width, max_height	= undefined;
-var renderCanvas			= undefined;
+addEventListener('load', generateElements);
 
-addEventListener("load", function() {
-  generateElements();
+function generateElements() {
+	// Create DOM structure
+	
+	var mainElement = document.getElementById('generator');
+	mainElement.innerHTML = "";
+	
+	let statusElement = document.createElement('p');
+	statusElement.textContent = "Select a file to upload.";
+	mainElement.appendChild(statusElement);
 
-});
-
-
-
-function generateElements(){
-  var mainElement = document.body;
-  mainElement.innerHTML = "";
-
-  let statusElement = document.createElement("p");
-  statusElement.textContent = "Select a file to upload.";
-  mainElement.appendChild(statusElement);
-
-  let fileUploadElement = document.createElement("input");
-  fileUploadElement.type = "file";
-  mainElement.appendChild(fileUploadElement);
-
-  let overlayImageElement = new Image();
-  overlayImageElement.src = "assets/img/overlay.png";
-  overlayImageElement.style.display = "none";
-  mainElement.appendChild(overlayImageElement);
-
-
-	fileUploadElement.addEventListener("change", function() {
+	let fileUploadElement = document.createElement('input');
+	fileUploadElement.type = 'file';
+	mainElement.appendChild(fileUploadElement);
+	
+	let overlayImageElement = new Image();
+	overlayImageElement.src = "assets/img/overlay.png";
+	overlayImageElement.style.display = 'none';
+	mainElement.appendChild(overlayImageElement);
+	
+	fileUploadElement.addEventListener('change', function() {
+		statusElement.textContent = "Uploading file...";
+		
 		if(this.files && this.files[0]) {
-			let img = document.createElement("img");
-			img.src = URL.createObjectURL(this.files[0]);
-
-			img.addEventListener("load", function(){
-        statusElement.textContent = "Processing...";
-
-      	scale_width 	= max_width 	/ this.width;
-      	scale_height	= max_height	/ this.height;
-
+			let uploadedImage = document.createElement("img");
+			uploadedImage.src = URL.createObjectURL(this.files[0]);
+			
+			uploadedImage.addEventListener('load', function() {
+				statusElement.textContent = "Processing...";
+				
+				// prepare canvas
+				let renderCanvas 		= document.createElement('canvas');
+				renderCanvas.width 	= overlayImageElement.width;
+				renderCanvas.height	= overlayImageElement.height;
+		
+				let ctx = renderCanvas.getContext('2d');
+				
+				// fill canvas with background color
+				ctx.beginPath();
+				ctx.rect(0, 0, renderCanvas.width, renderCanvas.height);
+				ctx.fillStyle = "#becdbf";
+				ctx.fill();
+				
+				// scale uploaded image
+				scale_width 	= renderCanvas.width 	/ uploadedImage.width;
+      	scale_height	= renderCanvas.height	/ uploadedImage.height;
+      	
       	scale = Math.min(scale_width, scale_height);
 
-      	new_width 	= this.width 	* scale;
-      	new_height	= this.height 	* scale;
+      	newWidth 	= uploadedImage.width 	* scale;
+      	newHeight	= uploadedImage.height	* scale;
+      	
+      	console.log(renderCanvas.width, renderCanvas.height, newWidth, newHeight, uploadedImage.width, uploadedImage.height);
 
-      	this.width 	= new_width;
-      	this.height = new_height;
-
-      	offset_left	= (max_width	- new_width) 	/ 2;
-      	offset_top	= (max_height 	- new_height) 	/ 2;
-
-      	let cvs 	= document.createElement("canvas");
-
-      	cvs.width 	= max_width;
-      	cvs.height 	= max_height;
-
-      	let ctx 	= cvs.getContext("2d");
-
-      	ctx.drawImage(this, offset_left, offset_top, this.width, this.height);
-      	profilePicture = ctx.getImageData(0, 0, max_width, max_height);
-
-        for (let i = 0; i < max_width * max_height; i += 1) {
-      		data = getPixel(imageData, i);
-      		if(data[3] == 0) {
-      			setPixel(imageData, i, getPixel(profilePicture, i));
-      		}
-      	}
-
-      	ctx = renderCanvas.getContext("2d");
-      	ctx.putImageData(imageData, 0, 0);
-
-      	// set image url to blob
+      	uploadedImage.width 	= newWidth;
+      	uploadedImage.height 	= newHeight;
+				offsetLeft	= (renderCanvas.width		- newWidth) 	/ 2;
+				offsetTop		= (renderCanvas.height 	- newHeight) 	/ 2;
+				
+				console.log(offsetLeft, offsetTop);
+				
+				// render uploadedImage and overlay
+				ctx.drawImage(uploadedImage, offsetLeft, offsetTop, newWidth, newHeight);
+				ctx.drawImage(overlayImageElement, 0, 0);
+				
+				// set image url to blob
         let downloadImageElement = document.createElement("img");
       	downloadImageElement.src = renderCanvas.toDataURL();
         mainElement.appendChild(downloadImageElement);
 
       	statusElement.textContent = "Done!";
 
+				// create downloadlink
         let downloadButtonElement = document.createElement("a");
         downloadButtonElement.innerText = "Download";
         downloadButtonElement.href = renderCanvas.toDataURL();
         downloadButtonElement.download = "image.png";
         mainElement.appendChild(downloadButtonElement);
-
-        fileUploadElement.parentElement.removeChild(fileUploadElement);
+        
+        // create recreate button and remove filechooser
+        mainElement.removeChild(fileUploadElement);
 
         let renewFormElement = document.createElement("button");
         renewFormElement.innerText = "Neues Bild generieren...";
@@ -91,41 +87,7 @@ function generateElements(){
           generateElements();
         });
         mainElement.appendChild(renewFormElement);
-      });
-
+			});
 		}
-	});
-
-
-	let cvs = document.createElement("canvas");
-
-  overlayImageElement.addEventListener("load", function(){
-    max_width 	= cvs.width 	= overlayImageElement.width;
-  	max_height 	= cvs.height 	= overlayImageElement.height;
-
-    let ctx = cvs.getContext("2d");
-
-  	ctx.drawImage(overlayImageElement, 0, 0, cvs.width, cvs.height);
-  	imageData = ctx.getImageData(0, 0, cvs.width, cvs.height);
-
-  	renderCanvas = cvs;
-  })
-
-}
-
-
-function getPixel(imgData, index) {
-	/**
-	 *
-	 */
-
-	return imgData.data.slice(index * 4, index * 4 + 4);
-}
-
-function setPixel(imgData, index, pixelData) {
-	/**
-	 *
-	 */
-
-	imgData.data.set(pixelData, index * 4);
+	}); 
 }
