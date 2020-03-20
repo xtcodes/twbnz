ded775c84c44e3f3f213af1df0d29115d81ca22b
SaturnHafen <saturnhafen@gmail.com>
2020-03-20 21:48:39 +0100

    add logos change generator to make it more modular create interface.js to make the ui-generator bridge

--- src/js/generator.js
+++ src/js/generator.js
@@ -1,111 +1,41 @@
-var config;
-
-addEventListener('load', function() {
-  const request = new Request('config/config.json');
-  
-  fetch(request)
-    .then(response => {
-      if(response.status == 200) {
-	return response.json();
-      } else {
-	throw new Error("Could not fetch config from server!");
-      }
-    }).then(json => {
-      config = json;
-      generateElements();
-    }).catch(error => {
-      console.log(error);
-    });
-});
-
-function generateElements() {
-	// Create DOM structure
+class Generator {
 	
-	var mainElement = document.getElementById(config.rootElementId);
-	mainElement.innerHTML = "";
+	static defaults = {
+		width: 200,
+		height: 200,
+		backgroundColor: '#ffffff',
+		offset: {
+			top: 0,
+			left: 0
+		},
+		scale: 1
+	};
 	
-	let statusElement = document.createElement('p');
-	statusElement.textContent = config.messages.status.startup;
-	mainElement.appendChild(statusElement);
-
-	let fileUploadElement = document.createElement('input');
-	fileUploadElement.type = 'file';
-	mainElement.appendChild(fileUploadElement);
-	
-	let overlayImageElement = new Image();
-	overlayImageElement.src = config.overlaySource;
-	overlayImageElement.style.display = 'none';
-	mainElement.appendChild(overlayImageElement);
-	
-	fileUploadElement.addEventListener('change', function() {
+	constructor(options) {
+		// create canvas
+		this.renderCanvas = document.createElement('canvas');
+		this.renderCanvas.width = options.width ? options.width : Generator.defaults.width;
+		this.renderCanvas.height = options.height ? options.height : Generator.defaults.height;
 		
-		if(this.files && this.files[0]) {
-		  statusElement.textContent = config.messages.status.uploading;
-		  let uploadedImage = document.createElement('img');
-		  uploadedImage.src = URL.createObjectURL(this.files[0]);
-			
-		  uploadedImage.addEventListener('load', function() {
-				statusElement.textContent = config.messages.status.processing;
-				
-				// prepare canvas
-				let renderCanvas 	= document.createElement('canvas');
-				renderCanvas.width 	= overlayImageElement.width;
-				renderCanvas.height	= overlayImageElement.height;
+		this.renderContext = this.renderCanvas.getContext('2d');
 		
-				let ctx = renderCanvas.getContext('2d');
-				
-				// fill canvas with background color
-				ctx.beginPath();
-				ctx.rect(0, 0, renderCanvas.width, renderCanvas.height);
-				ctx.fillStyle = config.backgroundColor;
-				ctx.fill();
-				
-				// scale uploaded image
-				scale_width 	= renderCanvas.width 	/ uploadedImage.width;
-      	scale_height	= renderCanvas.height	/ uploadedImage.height;
-      	
-      	scale = Math.min(scale_width, scale_height);
-
-      	newWidth 	= uploadedImage.width 	* scale;
-      	newHeight	= uploadedImage.height	* scale;
-      	
-      	console.log(renderCanvas.width, renderCanvas.height, newWidth, newHeight, uploadedImage.width, uploadedImage.height);
-
-      	uploadedImage.width 	= newWidth;
-      	uploadedImage.height 	= newHeight;
-				offsetLeft	= (renderCanvas.width		- newWidth) 	/ 2;
-				offsetTop		= (renderCanvas.height 	- newHeight) 	/ 2;
-				
-				console.log(offsetLeft, offsetTop);
-				
-				// render uploadedImage and overlay
-				ctx.drawImage(uploadedImage, offsetLeft, offsetTop, newWidth, newHeight);
-				ctx.drawImage(overlayImageElement, 0, 0);
-				
-				// set image url to blob
-        let downloadImageElement = document.createElement('img');
-      	downloadImageElement.src = renderCanvas.toDataURL();
-        mainElement.appendChild(downloadImageElement);
-
-      	statusElement.textContent = config.messages.status.done;
-
-				// create downloadlink
-        let downloadButtonElement = document.createElement('a');
-        downloadButtonElement.innerText = config.messages.buttons.download;
-        downloadButtonElement.href = renderCanvas.toDataURL();
-        downloadButtonElement.download = config.profilePictureName;
-        mainElement.appendChild(downloadButtonElement);
-        
-        // create recreate button and remove filechooser
-        mainElement.removeChild(fileUploadElement);
-
-        let renewFormElement = document.createElement('button');
-        renewFormElement.innerText = config.messages.buttons.newImage;
-        renewFormElement.addEventListener('click', function(){
-          generateElements();
-        });
-        mainElement.appendChild(renewFormElement);
-			});
-		}
-	}); 
+		// fill canvas with background color
+		this.renderContext.beginPath();
+		this.renderContext.rect(0, 0, this.renderCanvas.width, this.renderCanvas.height);
+		this.renderContext.fillStyle = options.backgroundColor ? options.backgroundColor : Generator.defaults.backgroundColor;
+		this.renderContext.fill();
+	}
+	
+	addLayer(image, options) {
+		let x = options.offset && options.offset.left ? options.offset.left : Generator.defaults.offset.left;
+		let y = options.offset && options.offset.top ? options.offset.top : Generator.defaults.offset.top;
+		
+		let scale = options.scale ? options.scale : Generator.defaults.scale;
+		
+		this.renderContext.drawImage(image, x, y, image.width * scale, image.height * scale);
+	}
+	
+	render() {
+		return this.renderCanvas.toDataURL();
+	}
 }
